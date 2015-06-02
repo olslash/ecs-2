67c5a61e3a8c1d3773caf9e2fe6b693b2d503722
Mitch R <olordhux+github@gmail.com>
2015-06-02 11:12:21 -0700

    test game

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/* @flow */
+'use strict';
+
+var e = require('./engine');
+var assemblages = require('./assemblages');
+
+var player = e.makeEntity(assemblages.player);
+// var s: System = {
+//   tick: function(components) {
+//
+//   }
+// }
